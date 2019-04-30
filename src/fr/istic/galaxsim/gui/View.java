3fd0620cc261c413f11b7a014c3d75c6667079a6
Alexis Gagoud <alexis.gagoud@hotmail.fr>
2019-04-30 18:50:58 +0200

    amélioration des noms de variables, modification des noms des classes et ajout d'un systeme pour ajouter des options au formulaire

--- src/fr/istic/galaxsim/gui/View.java
+++ src/fr/istic/galaxsim/gui/View.java
@@ -6,7 +6,10 @@ import javafx.event.EventHandler;
 import javafx.fxml.*;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.stage.WindowEvent;
@@ -24,9 +27,14 @@ public class View extends Application {
 		
 		
 		
+		//FXMLLoader fxmlloader = new FXMLLoader();
+		//Parent root = fxmlloader.load(getClass().getResource("Layout.fxml"));
 		
-		AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("GLayout.fxml"));
-		
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("Layout.fxml"));
+	    Parent root = loader.load();
+	    Layout mylayout = loader.getController();
+	    
+	    
 		Scene scene = new Scene(root, 100, 100);
 		stage.setTitle("GalaxSim");
 		stage.initStyle(StageStyle.DECORATED);
@@ -35,6 +43,12 @@ public class View extends Application {
 		stage.setWidth(800);
 		stage.setScene(scene);
 		stage.show();
+		
+		
+		
+		fillOption(mylayout);
+		
+		
 		
 		//quit actions
 		stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
@@ -46,6 +60,18 @@ public class View extends Application {
 		});
 				
 		
+	}
+	
+	
+	public void fillOption(Layout mylayout){
+		mylayout.addFormOption(new Rectangle(50,82));
+		TextField t = new TextField("info");
+		t.setMaxWidth(350);
+		mylayout.addFormOption(t);
+		mylayout.addFormOption(new Rectangle(20,30));
+		mylayout.addFormOption(new Label("hey, salut"));
+		mylayout.addFormOption(new Circle(23));
+		mylayout.addFormOption(new Rectangle(58,12));
 	}
 	
 	
